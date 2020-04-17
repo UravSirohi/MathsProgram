e5496cafac17c617f04979a44876b38230efed66
Urav Sirohi <urav.sirohi@outlook.com>
2020-04-17 17:25:45 +0100

    update

--- MathsProject/MathsProgram.py
+++ MathsProject/MathsProgram.py
@@ -2,6 +2,6 @@ from modules_009 import *
 
 print(f'''{Style.BRIGHT}{Fore.LIGHTCYAN_EX}If you need assistance type 'help' in the input line when the maths- 
 question-answer stage is present
-_______________________________________________________________________________''')
+______________________________________________________________________''')
 print(Style.NORMAL)
 set_restart()
